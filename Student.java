58c6eee63a3ef657dbab6a23cc1788e6e6fc3d4d
Hristina <tiinna.96@gmail.com>
2020-04-12 23:02:12 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -3,6 +3,13 @@ class Student {
 	String firstName;
 	String lastName;
 
+        ArrayList<int> labPonits = new ArrayList<int>(); 
+  
+        // Initialize an ArrayList with add() 
+        labPonits.add("5"); 
+        labPonits.add("7"); 
+        labPonits.add("5"); 
+
 	//TODO constructor
 
 	//TODO seters & getters
