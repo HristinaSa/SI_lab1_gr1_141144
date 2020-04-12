960062860c948582bf5474068ba394bca47e0a67
Hristina <tiinna.96@gmail.com>
2020-04-12 23:19:10 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -11,8 +11,16 @@ class Student {
         labPonits.add("5"); 
 
 	//TODO constructor
-
+	Student(){}
+	Student ( string index, string firstName, string lastName){
+		this->index(index);
+		this->firstName(firstName);
+		this->lastName(lastName);
+	}
 	//TODO seters & getters
+	string getIndex(){return Index; }
+	string getFirstName () {return firstName;}
+	string getLastName () {return lastName; }	
 
 	public double getAverage() {
 		//TODO
@@ -20,5 +28,13 @@ class Student {
 
 	public boolean hasSignature() {
 		//TODO
+		for(int i=0; i<=labPoints.lenght(); i++)
+		{
+			if(labPoints[i]>8)
+			{
+				return true;
+			}
+			else { return false; }
+		}
 	}
 }
